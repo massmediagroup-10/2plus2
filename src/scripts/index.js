c82b25934266508f2235017e7c9e17c2d74d25da
node-ist <node_ist@massmediagroup.pro>
2017-02-22 10:46:28 +0200

    product page done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -36,7 +36,6 @@ function navigation() {
 
     $('body').on({
         'mousewheel': function(e) {
-            console.log(e.target.className);
             if (e.target.className == 'overlay') {
                 e.preventDefault();
                 e.stopPropagation();
@@ -55,11 +54,144 @@ function headerSearch() {
     });
 }
 
+function initSliders() {
+    $('.detail-color').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        prevArrow: '<a href="#" class="slick-prev"><i class="fa fa-chevron-left"></i></a>',
+        nextArrow: '<a href="#" class="slick-next"><i class="fa fa-chevron-right"></i></a>',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+
+    $('.detail-slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.detail-slider-nav',
+    });
+    $('.detail-slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.detail-slider-for',
+        focusOnSelect: true,
+        arrows: false,
+        vertical: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                vertical: false
+            }
+        }, {
+            breakpoint: 780,
+            settings: {
+                slidesToShow: 5,
+                vertical: false
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 4,
+                vertical: false
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                vertical: false
+            }
+        }]
+    });
+}
+
+function tabs(block) {
+    if (typeof(block) === 'undefined') block = $('.tabs');
+    block.each(function() {
+        var $wrap = $(this);
+        if (!$wrap.is('.tabs-done')) {
+            $wrap.addClass('tabs-done');
+            $($wrap).on('click', '[data-tabId]', function(event) {
+                event.preventDefault();
+                var tabid = $(this).data('tabid');
+                $('[data-tabId]', $wrap).removeClass('active');
+                $('[data-tabId="' + tabid + '"]', $wrap).addClass('active');
+                $('[data-tab]', $wrap).removeClass('active').addClass('hidden');
+                $('[data-tab="' + tabid + '"]', $wrap).addClass('active').removeClass('hidden');
+            });
+            if ($('.active[data-tabId]', $wrap).length > 0)
+                $('.active[data-tabId]', $wrap).click();
+            else
+                $('[data-tabId]:eq(0)', $wrap).click();
+        }
+    });
+}
+
+function tabsResponsive() {
+    if (isMobile()) {
+        $('.tabs').each(function() {
+            var panel = $(this);
+            panel.find('.tabs-panel').each(function() {
+                var tab = $(this).data('tab');
+                var tabTitle = $('.tabs-title a[data-tabid="' + tab + '"]', panel);
+                if (!tabTitle.siblings('.tabs-panel').length) {
+                    var content = $(this).clone().addClass('responsive');
+                    tabTitle.after(content);
+                }
+            });
+        });
+    } else {
+        $('.tabs').each(function() {
+            var panel = $(this);
+            $('.tabs-title .tabs-panel', panel).remove();
+        });
+    }
+}
+
+function detailColor() {
+    $(document).on('click', '.detail-color-item a', function(e) {
+        e.preventDefault();
+        $('.detail-color-item a').removeClass('active');
+        $(this).addClass('active');
+
+        //set color to smwhr for processing
+        var color = $(this).data('color');
+    });
+}
+
+function detailSize() {
+    $(document).on('click', '.detail-size-item', function(e) {
+        e.preventDefault();
+        $('.detail-size-item').removeClass('active');
+        $(this).addClass('active');
+
+        //set size to smwhr for processing
+        var size = $(this).data('size');
+    });
+}
+
 $(document).ready(function() {
     foundation();
     footerplaceholder();
     navigation();
     headerSearch();
+    initSliders();
+    tabs();
+    tabsResponsive();
+    detailColor();
+    detailSize();
 
     $('form').each(function() {
         $(this).validate();
@@ -67,5 +199,6 @@ $(document).ready(function() {
 
     $(window).resize(function() {
         footerplaceholder();
+        tabsResponsive();
     });
 });
